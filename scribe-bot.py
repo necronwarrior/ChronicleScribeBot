0c54eccaf916177a7b26af542ba82ff911f0c447
Jack Baptie <necron_warrior@hotmail.co.uk>
2022-09-23 00:45:43 +0100

    Allows "/scribe <cardname>" as a command by checking the next arg to see if its a command; Changed "." to "/" for command calling; reworded help text to be more readable for non-programmers; resolved bug where underscores would not always be removed

--- scribe-bot.py
+++ scribe-bot.py
@@ -17,7 +17,7 @@ intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='.scribe ', intents=intents)
+bot = commands.Bot(command_prefix='/scribe', intents=intents)
 
 def findCardImage(name, path):
     """
@@ -43,10 +43,75 @@ async def on_ready():
         f'{guild.name}(id: {guild.id})'
     )
 
+####
+#
+# Because we accept "/scribe <cardname>" we have to manually check the next arg to see if the user wants another
+# command, or fallback to card if they want the shorthand
+#
+####
+
+PIPE_HELP_TEXT = f"If you type \".scribe <card name>\", I will seek out an image of " \
+                 f"the card if I can find it.\n"
+@bot.command(name='pipe', aliases=['', ' '])
+async def pipe(ctx, *args):
+    """
+    Takes in the first argument of " " and goes straight to card searching if there are no other protected arguments
+    next
+    """
+    if len(args) == 0:
+        await ctx.author.send(f"I require the name of a card, {ctx.author.name}.")
+        return
+
+    parsedArgs = [""] * (len(args)-1)
+    for i in range(0, len(args)-1):
+        parsedArgs[i] = args[i+1]
+
+    realCommand = args[0]
+    if realCommand == "cardlist":
+        await cardlist(ctx, parsedArgs)
+    elif realCommand == "card":
+        await card(ctx, parsedArgs)
+    elif realCommand == "commands":
+        await commands(ctx, parsedArgs)
+    else:
+        await card(ctx, args)
+
+CARD_HELP_TEXT = f"If you type \".scribe card <card name>\", I will seek out an image of " \
+                 f"the card if I can find it.\n"
+async def card(ctx, args):
+    """
+    Outputs the image of a card to the executed channel based on input card name.
+    Will prompt user if input is invalid.
+    """
+    if len(args) == 0:
+        await ctx.send(f"I require the name of a card, {ctx.author.name}.")
+        return
+
+    # Condense all card arguments into one full name with underscore seperators
+    cardString = "_".join(args)
+
+    cardString.replace(" ", "_")
+    fileLocation = findCardImage(cardString, os.getcwd() + "\\card_images\\")
+
+    cardString = cardString.replace("_", " ").lower().title()
+
+    if fileLocation == None:
+        await ctx.send(f"I find no record of the card \"{cardString}\" in my collection, {ctx.author.name}.")
+        return
+
+    with open(fileLocation + ".png", 'rb') as f:
+        picture = discord.File(f)
+
+        # We say behold if we're talking directly to a person for flavour
+        if isinstance(ctx.channel, discord.channel.DMChannel):
+            await ctx.channel.send(f"BEHOLD {ctx.author.name}, {cardString}!", file=picture)
+        else:
+            await ctx.channel.send(file=picture)
+        return
+
 CARDLIST_HELP_TEXT = f"If you type \".scribe cardlist <class name>\", I will message you a list of " \
                      f"all the cards for that class, if I can find it.\n"
-@bot.command(name='cardlist')
-async def cardlist(ctx, *args):
+async def cardlist(ctx, args):
     """
     Outputs a list of all cards that a class contains to a DM chat based on input class name.
     Will prompt user if input is invalid.
@@ -58,7 +123,7 @@ async def cardlist(ctx, *args):
     className = args[0].lower().title()
 
     if className not in ACCEPTED_CLASSES:
-        await ctx.author.send(f"I apologise, but I cannot find {className} in my collection, {ctx.author.name}.")
+        await ctx.author.send(f"I apologise, but I cannot find \"{className}\" in my collection, {ctx.author.name}.")
         return
 
     classCardList = os.listdir(os.getcwd() + f"\\card_images\\chronicle_rewritten_card_images\\{className}")
@@ -70,42 +135,17 @@ async def cardlist(ctx, *args):
     await ctx.author.send(f"{longCardString}")
     return 
 
-CARD_HELP_TEXT = f"If you type \".scribe card <card name>\", I will seek out an image of " \
-                 f"the card if I can find it.\n"
-@bot.command(name='card')
-async def card(ctx, *args):
-    """
-    Outputs the image of a card to the executed channel based on input card name.
-    Will prompt user if input is invalid.
-    """
-    if len(args) == 0:
-        await ctx.send(f"I require the name of a card, {ctx.author.name}.")
-        return
-
-    #Condense all card arguments into one full name with underscore seperators
-    cardString = '_'.join(args)
-
-    fileLocation = findCardImage(cardString, os.getcwd() + "\\card_images\\")
-    cardString.replace("_", " ")
-
-    if fileLocation == None:
-        await ctx.send(f"I find no record of the card {cardString} in my collection I'm afraid, {ctx.author.name}.")
-        return
-
-    with open(fileLocation + ".png", 'rb') as f:
-        picture = discord.File(f)
-        await ctx.channel.send(f"BEHOLD {ctx.author.name}, {cardString}!", file=picture)
-        return
-
 COMMANDHELP_HELP_TEXT = f"If you type \".scribe commands\", I will output this list of commands again.\n"
-@bot.command(name='commands')
 async def commandHelp(ctx, *args):
     """
     Outputs a list of documentation.
     """
+    if not isinstance(ctx.channel, discord.channel.DMChannel):
+        ctx.channel.send(f"I've messaged {ctx.author.name} a list of tasks I can perform.")
     helpString = "Here is a helpful list of user commands\n"
-    helpString += CARDLIST_HELP_TEXT + "\n"
+    helpString += PIPE_HELP_TEXT + "\n"
     helpString += CARD_HELP_TEXT + "\n"
+    helpString += CARDLIST_HELP_TEXT + "\n"
     helpString += COMMANDHELP_HELP_TEXT + "\n"
     await ctx.author.send(f"Here are all the tasks I can perform, {ctx.author.name}.")
     await ctx.author.send(f"{helpString}")
